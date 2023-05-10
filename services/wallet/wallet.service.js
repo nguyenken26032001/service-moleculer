1673c4a1090be9a10f0062716d63738836023dd3
Trần Văn Nguyện <kenn71917@gmail.com>
2023-05-10 10:00:26 +0700

    todo withDraw wallet

--- services/wallet/wallet.service.js
+++ services/wallet/wallet.service.js
@@ -1,75 +1,132 @@
 "use strict";
 const DbMixin = require("../../mixins/db.mixin");
+const { broker, Errors } = require("moleculer");
+const { MoleculerClientError } = Errors;
 module.exports = {
 	name: "wallet",
 	mixins: [DbMixin("wallet")],
 	settings: {
 		MODE_CHARGE: process.env.MODE_CHARGE,
 		MODE_WITH_DRAW: process.env.MODE_WITH_DRAW,
-		fields: ["_id", "createdAt", "quantity", "type", "mode"],
+		fields: [
+			"_id",
+			"userId",
+			"packageId",
+			"quantity",
+			"expiredDate",
+			"type",
+			"mode",
+			"createdAt",
+		],
 		entityValidator: {
-			createdAt: "number",
+			userId: "string",
+			packageId: "string",
 			quantity: "number",
+			expiredDate: "number",
 			type: "string",
 			mode: "string",
+			createdAt: "number",
 		},
 	},
 	actions: {
-		countToken_ExpireDate: {
-			rest: "GET /:userId",
-			auth: "require",
+		addTransaction: {
+			rest: "POST /add",
+			auth: "required",
+			params: {
+				packageId: { type: "string", required: true },
+			},
 			async handler(ctx) {
-				const walletForUser = await this.adapter.find({
+				const packageId = ctx.params.packageId;
+				const userId = ctx.meta.user._id;
+				//get info package by packageId
+				const dataPackage = await ctx.call("packages.getPackage", {
+					packageId: packageId, // <=> packageId
+				});
+				const updateWallet = {
+					userId: userId,
+					packageId: ctx.params.packageId,
+					packageName: dataPackage.packageName,
+					quantityToken: dataPackage.quantityToken,
+					expiredDate: dataPackage.expire,
+					type: dataPackage.packageName,
+					mode: this.settings.MODE_CHARGE,
+					createdAt: new Date().getTime(),
+				};
+				await ctx.emit("wallet.change", updateWallet); // event biến động ví token
+				await ctx.call("user.updateUserToken", { userId: userId });
+			},
+		},
+		InfoWallet: {
+			rest: "GET /info",
+			auth: "required",
+			async handler(ctx) {
+				return this.adapter.find({
 					query: {
-						userId: ctx.params.userId,
+						userId: ctx.meta.user._id,
 					},
 				});
-				let token = 0;
-				let expiredDate = 0;
-				walletForUser.map((item) => {
-					token += item.quantityToken;
-					expiredDate += item.expiredDate;
+			},
+		},
+		useService: {
+			rest: "POST /userService",
+			auth: "required",
+			params: {
+				serviceId: { type: "string", require: true },
+				time: { type: "number", require: true },
+			},
+			async handler(ctx) {
+				const dataService = await ctx.call("services.getService", {
+					serviceId: ctx.params.serviceId,
+				});
+				const updateWallet = {
+					userId: ctx.meta.user._id,
+					serviceId: ctx.params.serviceId,
+					serviceName: dataService.serviceName,
+					quantityToken: -Math.round(ctx.params.time / 1000),
+					createdAt: new Date().getTime(),
+					mode: this.settings.MODE_WITH_DRAW,
+				};
+				await ctx.emit("wallet.changeWithDraw", updateWallet);
+				await ctx.call("user.updateUserToken", {
+					userId: ctx.meta.user._id,
 				});
 				return {
-					totalToken: token,
-					expired: expiredDate,
+					code: 200,
+					message: "success",
 				};
 			},
 		},
 	},
 	events: {
-		"wallet.change"(ctx) {
-			const addWallet = {
-				userId: ctx.params.userId,
-				createdAt: ctx.params.createdAt,
-				expiredDate: ctx.params.expiredDate,
-				quantityToken: ctx.params.quantityToken,
-				type: ctx.params.type,
-				mode: ctx.params.mode,
-			};
-			this.adapter.insert(addWallet);
-		},
-		"wallet.changeToken": {
-			// service user truyền params == object user để sử  thông tin của user call action phía dưới
+		"wallet.change": {
 			async handler(ctx) {
-				//ctx.params== object user full information
-				var token = 0;
-				var expiredDate = 0;
-				const walletForUser = await this.adapter.find({
-					query: {
-						userId: ctx.params._id,
-					},
-				});
-				walletForUser.forEach((item) => {
-					token += item.quantityToken;
-					expiredDate += item.expiredDate;
-				});
-				const changeTokenUser = {
-					id: ctx.params._id,
-					token: token,
-					expiredDate: expiredDate,
+				console.log("into wallet change", ctx.params);
+				const addWallet = {
+					userId: ctx.params.userId,
+					packageId: ctx.params.packageId,
+					packageName: ctx.params.packageName,
+					quantityToken: ctx.params.quantityToken,
+					expiredDate: ctx.params.expiredDate,
+					createdAt: ctx.params.createdAt,
+					type: ctx.params.type,
+					mode: ctx.params.mode,
 				};
-				ctx.call("user.updateUserInfo", changeTokenUser);
+				// this._create(addWallet);
+				await this.adapter.insert(addWallet);
+			},
+		},
+		"wallet.changeWithDraw": {
+			async handler(ctx) {
+				const addWallet = {
+					userId: ctx.params.userId,
+					serviceId: ctx.params.serviceId,
+					serviceName: ctx.params.serviceName,
+					quantityToken: ctx.params.quantityToken,
+					createdAt: ctx.params.createdAt,
+					mode: ctx.params.mode,
+				};
+				// this._create(ctx, addWallet);
+				await this.adapter.insert(addWallet);
 			},
 		},
 	},
